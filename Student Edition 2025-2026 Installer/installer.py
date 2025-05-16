bf5d642b0ab5b74aeaca10c9a4c1734bbc3b343a
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-05-16 19:19:14 -0400

    Update installer.py

--- Student Edition 2025-2026 Installer/installer.py
+++ Student Edition 2025-2026 Installer/installer.py
@@ -1,4 +1,4 @@
-# This was made just if you need TS-COMPONENTS26 Student Edition 2025-2026 But Dwonloaded The Normal Version
+# This was made just if you need TS-COMPONENTS26 Student Edition 2025-2026 But Downloaded The Normal Version
 # This uses webbrowser to download the files
 import webbrowser
 
